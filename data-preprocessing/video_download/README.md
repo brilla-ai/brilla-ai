b6268e160237641747c5143a34c20d3561139b5e
KEVIN TAKYI YEBOAH <93127219+quaku21@users.noreply.github.com>
2023-03-20 16:55:54 +0000

    updates

---
+++ data-preprocessing/video_download/README.md
@@ -0,0 +1,9 @@
+This python script reads video links from a given sheet in an excel document, 
+downloads the videos and saves the videos to a google drive folder
+K.T Yeboah | 27-02-2023| 11:44am
+
+The instruction file (ins.json file) contains input variable parameters necessary to run the script.
+To run the script, first set the necesary parameters in the instruction file (ins.json) and save.
+The credentials.json file is a service file containing API keys, IDs and authorization credentials to the google drive folder in use. 
+To obtain a service file that points to a particular google drive folder, visit google cloud storage console (https://cloud.google.com/)
+Thereafter, invoke the python interpretor on the command line and add the name of the script=> py nsmq_vid.py
--- data-preprocessing/video_download/nsmq_vid.py
+++ data-preprocessing/video_download/nsmq_vid.py
@@ -11,49 +11,78 @@ from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 from googleapiclient.http import MediaFileUpload
 from pathlib import Path
+import json
+import os
 
 
-#Define doc variables
-excel_document_name = 'NSMQ.xlsx'  #document name
-excel_sheet_name = 'Sheet1'   #name of sheet in document
-videos_header_name = 'Links' #name of header of column containing video links
 
-# Read the Excel sheet and extract video links
-df = pd.read_excel(excel_document_name, excel_sheet_name)
-video_links = df[videos_header_name]
+#Function to read instruction file containing necessary parameters/values for this script to run
+def readInstructionFile():
+    with open('ins.json', 'r') as ins:
+        data = json.load(ins)
+    print(data)
+    return data
 
 
-# Download videos and save them to a local directory
-local_directory = 'videos/'
+def readExcelSheet(doc, sheet):
+    df = pd.read_excel(doc, sheet)
+    return df
 
-for link in video_links:
+
+def downloadVideo(link):
     try:
         yt = YouTube(link)
         video = yt.streams.filter(adaptive=True, file_extension='mp4').order_by('resolution').desc().first()
-        video.download(local_directory)
+        video.download(localVideoDirectoryPath)
+        print(f"video '{link}' downloaded succesfully")
     except Exception as e:
         print(f'Error downloading {link}: {e}')
 
 
+jsonData = readInstructionFile()
+
+#Define doc variables
+excelDocumentName = jsonData['excel_doc_name']  #document name
+excelSheetName = jsonData['sheet_name']  #name of sheet in document
+videosHeaderName = jsonData['video_header_name'] #name of header of column containing video links
+localVideoDirectoryPath = jsonData['video_folder_path'] #path to folder that will contain downloaded videos locally
+serviceAccountFilePath = jsonData['service_account_path'] #path to service account file 
+
+#check if local folder exists. !folder_exists? create it: continue
+if not os.path.exists(localVideoDirectoryPath):
+    os.makedirs(localVideoDirectoryPath)
+    print(f"Folder '{localVideoDirectoryPath}' does not exist. Folder created successfully")
+else:
+    print(f"Folder '{localVideoDirectoryPath}' already exists.")
+
+
+# Read the Excel sheet and extract video links
+df =readExcelSheet(excelDocumentName, excelSheetName)
+videoLinks = df[videosHeaderName]
+
+#Loop through list and download each video
+for link in videoLinks:
+    downloadVideo(link)
+
 # Authenticate with Google Drive API and create a folder to store the videos
-creds = service_account.Credentials.from_service_account_file('credentials.json')
+creds = service_account.Credentials.from_service_account_file(serviceAccountFilePath)
+driveService = build('drive', 'v3', credentials=creds)
 
-drive_service = build('drive', 'v3', credentials=creds)
+#name of local folder to contain donwload videos
+folder_name = jsonData['google_drive_folder_name']
 
-#name of local folder to contain donwloade videos
-folder_name = 'NSMQ Video Folder'
-
-file_metadata = {'name': folder_name, 'mimeType': 'application/vnd.google-apps.folder'}
-folder = drive_service.files().create(body=file_metadata, fields='id').execute()
-folder_id = folder.get('id')
+fileMetaData = {'name': folder_name, 'mimeType': 'application/vnd.google-apps.folder'}
+folder = driveService.files().create(body=fileMetaData, fields='id').execute()
+folder_id = folder.get('id') #can also be picked up from jsonData
 
 
 # Upload the videos to the Google Drive folder
-for video_path in Path(local_directory).glob('*.mp4'):
+for videoPath in Path(localVideoDirectoryPath).glob('*.mp4'):
     try:
-        file_metadata = {'name': video_path.name, 'parents': [folder_id]}
-        media = MediaFileUpload(str(video_path))
-        file = drive_service.files().create(body=file_metadata, media_body=media, fields='id').execute()
-        print(f'Successfully uploaded {video_path} to {folder_name} folder.')
+        print(f"Uploading '{videoPath}' from '{localVideoDirectoryPath}'")
+        fileMetaData = {'name': videoPath.name, 'parents': [folder_id]}
+        media = MediaFileUpload(str(videoPath))
+        file = driveService.files().create(body=fileMetaData, media_body=media, fields='id').execute()
+        print(f'Successfully uploaded {videoPath} to {folder_name} folder.') 
     except HttpError as error:
-        print(f'An error occurred: {error}')
+        print(f"An error occurred: '{error}' on '{videoPath}'")
