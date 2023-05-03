db94076446b470c16cfa0b6b99b86ec56c88065c
komlaRD <ankueric1@gmail.com>
2023-05-03 22:24:55 +0000

    Add demo video route to main route

--- webapp/backend/src/handlers/index.ts
+++ webapp/backend/src/handlers/index.ts
@@ -1,4 +1,5 @@
 import {Request, Response, Router} from "express";
+import demoVideos from "./api/videos";
 
 // Router object
 const routes = Router();
@@ -6,5 +7,8 @@ const routes = Router();
 routes.get("/", (req: Request, res: Response) => {
     res.send("Welcome to NSMQ AI Web backend")
 });
+
+// Demo video route
+routes.use("/videos", demoVideos);
 
 export default routes;
