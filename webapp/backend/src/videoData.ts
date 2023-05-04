965c12fc7fab11001c924159d37580138983cf4f
komlaRD <ankueric1@gmail.com>
2023-05-04 19:43:58 +0000

    Metadata for videos

---
+++ webapp/backend/src/videoData.ts
@@ -0,0 +1,15 @@
+const allVideos = [
+  {
+    id: "video1",
+    title:
+      "#NSMQ2022 PRELIMINARY STAGE _ ADU GYAMFI SHS VS ABURI GIRLS’ SHS VS SEFWI WIAWSO SHS_cropped",
+  },
+  {
+    id: "video2",
+    title:
+      "#NSMQ2022 PRELIMINARY STAGE _ O.L. OF MT. CARMEL GIRLS’ VS KUMASI WESLEY GIRLS’ _cropped",
+  },
+  { id: "video3", title: "NSMQ2022_PRESEC_ACCRAADEMY_POPEJOHNS_cropped" },
+];
+
+export default allVideos;
