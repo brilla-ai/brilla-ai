25e8094712ff2e9e8200e6e4afc1b047cc378844
komlaRD <ankueric1@gmail.com>
2023-05-04 19:49:45 +0000

    Configure video streaming routes

---
+++ webapp/backend/src/handlers/api/videos.ts
@@ -0,0 +1,63 @@
+import { Request, Response, Router } from "express";
+import fs from "fs";
+import allVideos from "../../videoData";
+import path from "path";
+
+// Initialise router object
+const demoVideos = Router();
+
+// Metadata for all videos
+demoVideos.get("/", (req: Request, res: Response) => {
+  res.json(allVideos);
+});
+
+// Metadata for a single video
+demoVideos.get("/:id/metadata", (req: Request, res: Response) => {
+  const id = parseInt(req.params.id, 10);
+  res.json(allVideos[id]);
+});
+
+// Endpoint for a single video to stream
+demoVideos.get("/video/:id", (req: Request, res: Response) => {
+    try{
+        // Constant range
+        const range = req.headers.range;
+
+        // Request parameter id
+        const id = req.params.id;
+
+        // Video path
+        const videoPath = path.join(__dirname, `../../../assets/${id}.mp4`);
+
+        // Size of the video
+        const videoSize = fs.statSync(videoPath).size;
+
+        // Set chunk size
+        const chunkSize = 1 * 1e6;
+
+        // Const start and end of video
+        const start = range ? Number(range?.replace(/\D/g, "")) : 0;
+        const end = Math.min(start + chunkSize, videoSize - 1);
+
+        // Content length
+        const contentLength = end - start + 1;
+
+        // Headers for playing video
+        const headers = {
+            "Content-Range": `bytes ${start} - ${end}/${videoSize}`,
+             "Accept-Ranges": "bytes",
+            "Content-Length": contentLength,
+            "Content-Type": "video/mp4",
+         };
+
+        res.writeHead(206, headers);
+
+        // Create video stream and pipe it to the response
+        const stream = fs.createReadStream(videoPath, { start, end });
+        stream.pipe(res);
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+export default demoVideos;
