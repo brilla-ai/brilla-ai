7288930937f8a3a960b6b648f7e5a6a69ef9d7ad
komlaRD <ankueric1@gmail.com>
2023-05-04 19:51:49 +0000

    Comment about main route

--- webapp/backend/src/handlers/index.ts
+++ webapp/backend/src/handlers/index.ts
@@ -4,6 +4,7 @@ import demoVideos from "./api/videos";
 // Router object
 const routes = Router();
 
+// Main route
 routes.get("/", (req: Request, res: Response) => {
     res.send("Welcome to NSMQ AI Web backend")
 });
