94d695548672ff23a8d8ee06afc1847308c3c539
Kelvin Amoaba <97001695+AmoabaKelvin@users.noreply.github.com>
2023-05-16 19:09:09 +0000

    Common footer and menubar (#23)
    * chore: install react icons
    
    * chore: add configurations for babel
    
    * feat: add logo for KwameAI
    
    * feat: add Nunito font
    
    * feat: extend tailwind design by adding custom colors and fonts
    
    * fix: remove unused stylings and set base style
    for page body
    
    * feat: wrap page with `Layout` component
    
    * feat: layout component which will be the parent
    for all pages, hence giving them the common footer and header
    
    * feat: add Header component
    
    * feat: add dummy Footer component
    footer will be implemented later, currently, just a base for the
    `Layout` component
    
    * feat: options button SVG component
    
    * fix: uncomment import and usage of `Footer`
    component
    
    * feat: update footer component to match figma design
    
    * chore: remove package-lock from VCS
    
    * chore: untrack package-lock file
    
    * style: add new line  at the bottom of file
    
    * update links on Footer.tsx

---
+++ webapp/frontend/src/components/layout/Header.tsx
@@ -0,0 +1,59 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const navigationPages = [
+  {
+    name: "Home",
+    href: "/",
+  },
+  {
+    name: "Demo Page",
+    href: "/demo",
+  },
+  {
+    name: "About",
+    href: "/about",
+  },
+];
+
+const Header = () => {
+  return (
+    <nav className="bg-grey border-gray-200 max-w-7xl mx-auto mt-4 rounded shadow-md px-6">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <div className="flex items-center space-x-4">
+          <Image
+            src="/KwameAILogo.png"
+            alt="Kwame AI Logo"
+            width={20}
+            height={20}
+          />
+          <span className="font-semibold text-xl tracking-tight">
+            NSMQ - KWAME AI
+          </span>
+        </div>
+
+        <button
+          data-collapse-toggle="navbar-default"
+          type="button"
+          className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+        ></button>
+        <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg  md:flex-row md:space-x-8 md:mt-0 md:border-0  text-black">
+            {navigationPages.map((page) => (
+              <li key={page.name}>
+                <Link
+                  href={page.href}
+                  className="block py-2 pl-3 pr-4  rounded md:bg-transparent  md:p-0 dark: "
+                >
+                  {page.name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
---
+++ webapp/frontend/src/components/layout/Layout.tsx
@@ -0,0 +1,19 @@
+// Layout component, which is used to wrap all the pages
+// It contains the header and footer
+
+import React from "react";
+
+import Footer from "./Footer";
+import Header from "./Header";
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
