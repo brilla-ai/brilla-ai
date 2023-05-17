2de9f12a87aa416aeacb0a655916124d02ca2f9c
Kelvin Amoaba <97001695+AmoabaKelvin@users.noreply.github.com>
2023-05-17 01:17:53 +0000

    feat: add video components to homepage

--- webapp/frontend/src/pages/index.tsx
+++ webapp/frontend/src/pages/index.tsx
@@ -1,21 +1,19 @@
+import VideoCard from "@/components/homepage/VideoCard";
 import Layout from "@/components/layout/Layout";
 
 export default function Home() {
   return (
     <Layout>
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <div className="mx-auto max-w-2xl lg:mx-0">
-          <h2 className="text-4xl font-bold tracking-tight sm:text-6xl">
-            NSMQ AI Challenge
-          </h2>
-          <p className={"mt-6 text-lg leading-8 "}>
-            {
-              "Can an AI win Ghana's National Science and Maths Quiz?. This is the question posed by the NSMQ AI Grand Challenge which is an AI Grand Challenge for Education using Ghana's National Science and Maths Quiz competition (NSMQ) as a case study."
-            }
-            {
-              "The goal of nsmqai is build an AI to compete live in the NSMQ competition and win — performing better than the best contestants in all rounds and stages of the competition."
-            }
-          </p>
+      <main className="flex min-h-screen flex-col items-center justify-between px-24 py-8">
+        <div>
+          <h5 className="tracking-tight text-center text-[30px]">
+            Demo Videos
+          </h5>
+          <div className="mt-4 grid grid-cols-1 md:grid-cols-3 gap-8 w-full">
+            <VideoCard id={1} image="/thumbnail.png" />
+            <VideoCard id={2} image="/thumbnail.png" />
+            <VideoCard id={3} image="/thumbnail.png" />
+          </div>
         </div>
       </main>
     </Layout>
