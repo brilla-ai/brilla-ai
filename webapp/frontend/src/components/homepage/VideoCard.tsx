0baf68a30d3bcf04e7397da2434a6272ad26afc3
Kelvin Amoaba <97001695+AmoabaKelvin@users.noreply.github.com>
2023-05-17 01:18:05 +0000

    feat: video card component

---
+++ webapp/frontend/src/components/homepage/VideoCard.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+
+type VideoCardProps = {
+  id: number;
+  image: string;
+};
+
+const VideoCard = ({ id, image }: VideoCardProps) => {
+  return (
+    <div className="max-w-sm bg-transparent  text-black">
+      <img className="w-full" src={image} alt={`Video number ${id}`} />
+      <div className="flex justify-center my-5">
+        <Link
+          href={`/demos/${id}`}
+          className="inline-flex items-center px-5 py-2 text-md font-medium text-center text-white bg-blue-600 rounded-lg hover:bg-blue-800"
+        >
+          Try this out
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default VideoCard;
