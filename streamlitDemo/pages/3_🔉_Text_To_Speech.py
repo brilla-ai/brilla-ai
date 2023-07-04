f563754218aa64b31f89d80c105545cef1faa4ec
William Edor <wedor3@gatech.edu>
2023-07-04 18:37:02 -0400

    - added an API setup page to be able to load the URLs. It persists the data in the environment variables so we don't have to refresh the app when we the urls change - integrated QA API and added options to select a sample riddle. QA page is now using the API and no mock data - integrated STT API. STT page is now using the STT API for both the audio sample and the live recording. Removed the whisper model previously being used as well - STT Page also autoplays the audio once the page loads. I did not find a way to hide the play/pause controls as well but I believe the autoplay is okay - replaced audioChunk folder with temp directory

--- streamlitDemo/pages/3_🔉_Text_To_Speech.py
+++ streamlitDemo/pages/3_🔉_Text_To_Speech.py
@@ -1,24 +1,19 @@
 import streamlit as st
-from utility import DEMO_QA_ANSWER, DEMO_AUDIO_1_PATH, get_tts_audio
+from utility import DEMO_AUDIO_1_PATH, get_tts_audio
 
 st.set_page_config(page_title="Text To Speech", page_icon="🔉", layout="wide")
 
 st.markdown("# Text To Speech Demo🔉")
 st.write(
     """
-    This demo illustrates a real time operation of the Text To Speech Model of the NSMQ AI. Steps:
-    - Enter Text
-    - Click "Get Generated Speech!"
-    - Play the real time generated audio
-    
-    Enjoy!
+    This demo illustrates a real time operation of the Text To Speech Model of the NSMQ AI.
     """
 )
 
 st.divider()
 
 # text input
-answer = st.text_area("Enter Text", DEMO_QA_ANSWER['answer'], placeholder = DEMO_QA_ANSWER['answer'], height = 80)
+answer = st.text_area("Enter Text", '', height = 80)
 
 # generated audio file
 if st.button('Get Generated Speech!'):
--- streamlitDemo/utility.py
+++ streamlitDemo/utility.py
@@ -1,13 +1,13 @@
 import streamlit as st
 import requests
 from pydub import AudioSegment
-import whisper
 import os
 import tempfile
-import glob
 from pathlib import Path
 from streamlit_webrtc import WebRtcMode, webrtc_streamer
 import queue
+import base64
+import json
 
 # VIDEO PATHS
 DEMO_VIDEO_1_PATH = './assets/video/video1.mp4'
@@ -19,156 +19,199 @@ DEMO_AUDIO_1_PATH  = './assets/audio/audio_video1.mp3'
 DEMO_AUDIO_2_PATH  = './assets/audio/audio_video2.mp3'
 DEMO_AUDIO_3_PATH  = './assets/audio/audio_video3.mp3'
 
-# AUDIO CHUNK FOLDER
-AUDIO_CHUNK_FOLDER_PATH = './audioChunks'
-
 # API ENDPOINTS
-STT_API = ''
-QA_API = ''
-TTS_API = ''
+STT_API_KEY = 'STT_API'
+QA_API_KEY = 'QA_API'
+TTS_API_KEY = 'TTS_API'
+NO_API_SET_FLAG = '-1'
 
 # TEST VALUES
-DEMO_QA_QUESTION = {"text": "there is really nothing improper about me,i am just a fraction,my numerator exceeds my denominator,i am not a mixed fraction or mixed number,an example of me is 7 3"}
-DEMO_QA_ANSWER = {"answer": "improper fraction"}
+QA_QUESTION_BANK = {
+    "riddle1": "there is really nothing improper about me,i am just a fraction,my numerator exceeds my denominator,i am not a mixed fraction or mixed number,an example of me is 7 3",
+    "riddle2": "I am a point of concurrency associated with a triangle. I am a meeting point of three lines associated with a triangle. I am in a way a center of some sort. I am not the in-center and neither am I the circum-center. I am the point at which the three altitudes of a triangle meet. Who am I?",
+    "riddle3": "I am a theorem in mechanics That is named after a French scientist named Bernard. I apply to the equilibrium state of an object. I give the relationship between three coplanar concurrent forces that act on a body in equilibrium. I recall the sine rule. Who am I?"
+    }
 
 # DEFAULTS FOR REAL TIME AUDIO RECORDING TRANSCRIPTION
-WHISPER_MODEL="base"        # "Model to use". Choices are ["tiny","base","small","medium","large"]
-USE_ENGLISH=False           # "Whether to use English model"
-TIMEOUT=3                   # Timeout for getting frames from the audio receiver. Default is 3 seconds.
+TIMEOUT = 3  # Timeout for getting frames from the audio receiver. Default is 3 seconds.
 
 # API FUNCTIONS
-def get_stt_text():
-    response = requests.get(STT_API)
-    return response.json()
+def get_stt_transcript(audioFile):
+    STT_API = get_stt_api()
+
+    # only make the API call if a valid url is present
+    if STT_API != NO_API_SET_FLAG:
+        STT_TRANSCRIPT_API = STT_API.rstrip('/') + '/get-transcript'
+        with open(audioFile, "rb") as audio_file:
+            audio_bytes = audio_file.read()
+            bytes_data = base64.b64encode(audio_bytes).decode()
+
+            # Create payload
+            payload = {"data": bytes_data, "filename": "temp_audio.wav"}
+            payload = json.dumps(payload)
+
+            # Send GET request to API endpoint
+            response = requests.get(STT_TRANSCRIPT_API, data=payload)
+
+            if response.status_code == 200:
+                transcript = response.json()['transcript']
+                return transcript
 
 def get_qa_answer(question):
-    response = requests.get(QA_API, data=question)
-    return response.json()['answer']
+    QA_API = get_qa_api()
+
+    # only make the API call if a valid url is present
+    if QA_API != NO_API_SET_FLAG:
+        question = json.dumps({'text' : question})
+        response = requests.get(QA_API.rstrip('/') + '/answer', data=question)
+
+        if response.status_code == 200:
+            answer = response.json()['answer']
+            return answer
 
 def get_tts_audio(answer):
-    response = requests.get(TTS_API, data=answer)
-    #TODO: change from json to audio
-    return response.json()
+    TTS_API = get_tts_api()
+    # only make the API call if a valid url is present
+    if TTS_API != NO_API_SET_FLAG:
+        response = requests.get(TTS_API, data=answer)
+        #TODO: change from json to audio
+        return response.json()
+
+# SET API IN ENVIRONMENT VARIABLES
+def set_stt_api(apiVal):
+    os.environ[STT_API_KEY] = apiVal
+
+def set_qa_api(apiVal):
+    os.environ[QA_API_KEY] = apiVal
+
+def set_tts_api(apiVal):
+    os.environ[TTS_API_KEY] = apiVal
+
+# GET API IN ENVIRONMENT VARIABLES
+def get_stt_api():
+    return os.environ.get(STT_API_KEY, NO_API_SET_FLAG)
+
+def get_qa_api():
+    return os.environ.get(QA_API_KEY, NO_API_SET_FLAG)
+    
+def get_tts_api():
+    return os.environ.get(TTS_API_KEY, NO_API_SET_FLAG)
+
+# AUTOPLAY AUDIO
+def autoplay_audio(audioFile):
+    with open(audioFile, "rb") as f:
+        data = f.read()
+        b64 = base64.b64encode(data).decode()
+        md = f"""
+            <audio controls autoplay="true">
+            <source src="data:audio/mp3;base64,{b64}" type="audio/mp3">
+            </audio>
+            """
+        st.markdown(
+            md,
+            unsafe_allow_html=True,
+        )
 
 # LOCAL STT PROCESSING
-def realTimeAudioFileSTT(audio_file_path):
-    # TODO: get actual transcription from API and display
-    # clean up audio chunk folder in case there is anything in there
-    cleanupAudioChunkFiles()
+def realtime_audio_file_STT(audio_file_path):
+    with st.spinner('Transcribing'):
+        # creating a placeholder for the fixed sized textbox
+        transcriptBox = st.empty()
+        transcriptText = ''
+        transcriptBox.text_area(
+            "Real time transcription",
+            transcriptText,
+            label_visibility  = 'hidden'
+        )
 
-    # creating a placeholder for the fixed sized textbox
-    transcriptBox = st.empty()
-    transcriptText = ''
-    boxHeight = 180
-    transcriptBox.text_area(
-        "Real time transcription",
-        transcriptText,
-        height = boxHeight,
-        label_visibility  = 'hidden'
-    )
+        # temp location for audio chunks
+        temp_dir = tempfile.mkdtemp()
+        audio_chunk_temp_file = os.path.join(temp_dir, "temp.wav")
 
-    # setup whisper models
-    whisperModel = whisper.load_model("base")
-
-    # majority of audio chunk code referenced from https://www.geeksforgeeks.org/audio-processing-using-pydub-and-google-speechrecognition-api/#
-    
-    # Input audio file to be sliced
-    audio = AudioSegment.from_mp3(audio_file_path)
-
-    # Length of the audiofile in milliseconds
-    n = len(audio)
-
-    # Variable to count the number of sliced chunks
-    counter = 1
-
-    # Interval length at which to slice the audio file.
-    # If length is 22 seconds, and interval is 5 seconds,
-    # The chunks created will be:
-    # chunk1 : 0 - 5 seconds
-    # chunk2 : 5 - 10 seconds
-    # chunk3 : 10 - 15 seconds
-    # chunk4 : 15 - 20 seconds
-    # chunk5 : 20 - 22 seconds
-    interval = 5 * 1000
-
-    # Length of audio to overlap.
-    # If length is 22 seconds, and interval is 5 seconds,
-    # With overlap as 1.5 seconds,
-    # The chunks created will be:
-    # chunk1 : 0 - 5 seconds
-    # chunk2 : 3.5 - 8.5 seconds
-    # chunk3 : 7 - 12 seconds
-    # chunk4 : 10.5 - 15.5 seconds
-    # chunk5 : 14 - 19.5 seconds
-    # chunk6 : 18 - 22 seconds
-    overlap = 0
-
-    # Initialize start and end seconds to 0
-    start = 0
-    end = 0
-    
-    # Flag to keep track of end of file.
-    # When audio reaches its end, flag is set to 1 and we break
-    flag = 0
-
-    # Iterate from 0 to end of the file,
-    # with increment = interval
-    for i in range(0, 2 * n, interval):
+        # majority of audio chunk code referenced from https://www.geeksforgeeks.org/audio-processing-using-pydub-and-google-speechrecognition-api/#
         
-        # During first iteration,
-        # start is 0, end is the interval
-        if i == 0:
-            start = 0
-            end = interval
-    
-        # All other iterations,
-        # start is the previous end - overlap
-        # end becomes end + interval
-        else:
-            start = end - overlap
-            end = start + interval
-    
-        # When end becomes greater than the file length,
-        # end is set to the file length
-        # flag is set to 1 to indicate break.
-        if end >= n:
-            end = n
-            flag = 1
-    
-        # Storing audio file from the defined start to end
-        chunk = audio[start:end]
-    
-        # Filename / Path to store the sliced audio
-        # extracting just the name of the file out of the path
-        filename = AUDIO_CHUNK_FOLDER_PATH + './' + os.path.splitext(os.path.basename(audio_file_path))[0] + '_chunk' + str(counter) + '.mp3'
-    
-        # Store the sliced audio file to the defined path
-        chunk.export(filename, format ="mp3")
-    
-        # Increment counter for the next chunk
-        counter = counter + 1
+        # Input audio file to be sliced
+        audio = AudioSegment.from_mp3(audio_file_path)
+
+        # Length of the audiofile in milliseconds
+        n = len(audio)
+
+        # Interval length at which to slice the audio file.
+        # If length is 22 seconds, and interval is 5 seconds,
+        # The chunks created will be:
+        # chunk1 : 0 - 5 seconds
+        # chunk2 : 5 - 10 seconds
+        # chunk3 : 10 - 15 seconds
+        # chunk4 : 15 - 20 seconds
+        # chunk5 : 20 - 22 seconds
+        # 5seconds proved to be the best interval to make the transcription correspond to the audio which is autoplayed in the UI
+        interval = 5 * 1000
+
+        # Length of audio to overlap.
+        # If length is 22 seconds, and interval is 5 seconds,
+        # With overlap as 1.5 seconds,
+        # The chunks created will be:
+        # chunk1 : 0 - 5 seconds
+        # chunk2 : 3.5 - 8.5 seconds
+        # chunk3 : 7 - 12 seconds
+        # chunk4 : 10.5 - 15.5 seconds
+        # chunk5 : 14 - 19.5 seconds
+        # chunk6 : 18 - 22 seconds
+        overlap = 0
+
+        # Initialize start and end seconds to 0
+        start = 0
+        end = 0
         
-        # Slicing of the audio file is done.
+        # Flag to keep track of end of file.
+        # When audio reaches its end, flag is set to 1 and we break
+        flag = 0
 
-        # transcribe model
-        transcription = whisperModel.transcribe(filename)
-        transcriptText += transcription['text']
-        transcriptBox.text_area("", transcriptText, height = boxHeight)
+        # Iterate from 0 to end of the file,
+        # with increment = interval
+        for i in range(0, 2 * n, interval):
+            
+            # During first iteration,
+            # start is 0, end is the interval
+            if i == 0:
+                start = 0
+                end = interval
+        
+            # All other iterations,
+            # start is the previous end - overlap
+            # end becomes end + interval
+            else:
+                start = end - overlap
+                end = start + interval
+        
+            # When end becomes greater than the file length,
+            # end is set to the file length
+            # flag is set to 1 to indicate break.
+            if end >= n:
+                end = n
+                flag = 1
+        
+            # Storing audio file from the defined start to end
+            sound_chunk = audio[start:end]
 
-        # Check for flag.
-        # If flag is 1, end of the whole audio reached.
-        # Close the file and break.
-        if flag == 1:
-            break
+            # Store the sliced audio file
+            sound_chunk.export(audio_chunk_temp_file, format ="wav")
+            
+            # Slicing of the audio file is done. transcribe audio chunks
+            transcriptText += get_stt_transcript(audio_chunk_temp_file)
+            os.remove(audio_chunk_temp_file)
 
-    # clean up audio chunk folder since processing is complete
-    cleanupAudioChunkFiles()
+            transcriptBox.text_area("", transcriptText)
 
-def realTimeAudioRecordingSTT(model=WHISPER_MODEL, english=USE_ENGLISH, timeout=TIMEOUT):
-    #there are no english models for large
-    if model != "large" and english:
-        model = model + ".en"
-    audio_model = whisper.load_model(model)
+            # Check for flag.
+            # If flag is 1, end of the whole audio reached.
+            if flag == 1:
+                break
+
+def realtime_audio_recording_STT():
+    # temp location for audio chunks
+    temp_dir = tempfile.mkdtemp()
+    audio_chunk_temp_file = os.path.join(temp_dir, "recording_temp.wav")
 
     # streamlit-webrtc component containing the audio functionality
     webrtc_ctx = webrtc_streamer(
@@ -178,62 +221,43 @@ def realTimeAudioRecordingSTT(model=WHISPER_MODEL, english=USE_ENGLISH, timeout=
         media_stream_constraints={"video": False, "audio": True},
     )
 
-    status_indicator = st.empty()
-
     # creating a placeholder for the fixed sized textbox
-    transcriptBox = st.empty()
-    # transcriptText = ''
-    # boxHeight = 180
-    # transcriptBox.text_area(
-    #     "Real time transcription",
-    #     transcriptText,
-    #     height = boxHeight,
-    #     label_visibility  = 'hidden'
-    # )
+    with st.spinner("Running..Say Something!"):
+        transcriptBox = st.empty()
+        transcriptText = ''
+        transcriptBox.text_area(
+            "Real time transcription",
+            transcriptText,
+            label_visibility  = 'hidden'
+        )
+    
+        while webrtc_ctx.state.playing:
+            if webrtc_ctx.audio_receiver:
+                try:
+                    audio_frames = webrtc_ctx.audio_receiver.get_frames(timeout=TIMEOUT)
+                except queue.Empty:
+                    continue
 
-    while True:
-        if webrtc_ctx.audio_receiver:
-            try:
-                audio_frames = webrtc_ctx.audio_receiver.get_frames(timeout=timeout)
-            except queue.Empty:
-                continue
-
-            status_indicator.write("Running...Say something!")
-            sound_chunk = AudioSegment.empty()
-            for audio_frame in audio_frames:
-                sound = AudioSegment(
-                    data=audio_frame.to_ndarray().tobytes(),
-                    sample_width=audio_frame.format.bytes,
-                    frame_rate=audio_frame.sample_rate,
-                    channels=len(audio_frame.layout.channels),
-                )
-                sound_chunk += sound
-            
-            if len(sound_chunk) > 0:
-                text = transcribeAudio(audio_model, sound_chunk)
-                transcriptBox.write(text)
-                # transcriptBox.text_area("", transcriptText, height = boxHeight)
-        else:
-            break
-
-def transcribeAudio(audio_model, audio_segment: AudioSegment):
-    with tempfile.NamedTemporaryFile(suffix=".wav", delete=False) as tmpfile:
-        audio_segment.export(tmpfile.name, format="wav")
-        answer = audio_model.transcribe(tmpfile.name, language='english')["text"]
-        tmpfile.close()  
-        os.remove(tmpfile.name)
-        return answer
-
-# CLEAN UP OF AUDIO CHUNK FILES
-def cleanupAudioChunkFiles():
-    for f in Path(AUDIO_CHUNK_FOLDER_PATH).glob('*.mp3'):
-        try:
-            f.unlink()
-        except OSError as e:
-            print("Error: %s : %s" % (f, e.strerror))
+                sound_chunk = AudioSegment.empty()
+                for audio_frame in audio_frames:
+                    sound = AudioSegment(
+                        data=audio_frame.to_ndarray().tobytes(),
+                        sample_width=audio_frame.format.bytes,
+                        frame_rate=audio_frame.sample_rate,
+                        channels=len(audio_frame.layout.channels),
+                    )
+                    sound_chunk += sound
+                
+                if len(sound_chunk) > 0:
+                    sound_chunk.export(audio_chunk_temp_file, format ="wav")
+                    transcriptText += get_stt_transcript(audio_chunk_temp_file)
+                    transcriptBox.text_area("", transcriptText)
+                    os.remove(audio_chunk_temp_file)
+            else:
+                break
 
 # OVERALL END TO END OPERATION DISPLAY
-def aiOperation(video_file_path, audio_file_path):
+def ai_operation(video_file_path, audio_file_path):
     videoCol, rttCol = st.columns([3,2])
 
     with videoCol:
@@ -243,7 +267,7 @@ def aiOperation(video_file_path, audio_file_path):
 
     with rttCol:
         if st.button('Transcribe'):
-            realTimeAudioFileSTT(audio_file_path)
+            realtime_audio_file_STT(audio_file_path)
 
     st.divider()
 
@@ -253,13 +277,13 @@ def aiOperation(video_file_path, audio_file_path):
         st.write("#### Question")
         # st.write(QA_QUESTION["text"])
         # st.write(get_stt_text()['reference'])
-        st.text_area("Question", DEMO_QA_QUESTION["text"], label_visibility  = 'hidden')
+        st.text_area("Question", QA_QUESTION_BANK["text"], label_visibility  = 'hidden')
         
     with answerCol:
         st.write("#### Answer")
         # st.write(QA_ANSWER["answer"])
         # st.write(get_qa_answer(question))
-        st.text_area("Answer", DEMO_QA_ANSWER["answer"], label_visibility  = 'hidden')
+        st.text_area("Answer", "answer", label_visibility  = 'hidden')
     
     st.divider()
 
