8909f49b47216aed6048b52e8bd0c2f2744c5bd9
wsedor <wsedor@wpi.edu>
2023-07-05 15:05:02 -0400

    integrate tts api (#43)
    * integrating TTS API
    
    * integrating TTS API

--- streamlitDemo/pages/3_🔉_Text_To_Speech.py
+++ streamlitDemo/pages/3_🔉_Text_To_Speech.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from utility import DEMO_AUDIO_1_PATH, get_tts_audio
+from utility import get_tts_audio, autoplay_audio, TTS_VOICE_BANK
 
 st.set_page_config(page_title="Text To Speech", page_icon="🔉", layout="wide")
 
@@ -12,13 +12,22 @@ st.write(
 
 st.divider()
 
+# placeholder for error message
+errorMsg = st.empty()
+
+# choose voice options
+voice = st.radio('Select A Voice', (TTS_VOICE_BANK['voice1'], TTS_VOICE_BANK['voice2']), horizontal=True)
+
 # text input
-answer = st.text_area("Enter Text", '', height = 80)
+text = st.text_area("Enter Text", '', height = 80)
 
 # generated audio file
 if st.button('Get Generated Speech!'):
-    # TODO: get actual audio from API and display
-    #st.write(get_tts_audio(answer))
-    audio_file = open(DEMO_AUDIO_1_PATH, 'rb')
-    audio_bytes = audio_file.read()
-    st.audio(audio_bytes, format="audio/mp3")
+    # check that text box has a value
+    if text:
+        outputAudioFile = ''
+        with st.spinner('Generating speech...'):
+            outputAudioFile = get_tts_audio(text, voice)
+        autoplay_audio(outputAudioFile)
+    else:
+        errorMsg.error('Please provide a text', icon="⚠️")
