82ab160012d9607b8b4742b74d5452a47201c3e2
Saket Kaswa <105808363+SaketKaswa20@users.noreply.github.com>
2023-07-31 21:11:06 +0530

    Update index.tsx

--- webapp/frontend/src/pages/index.tsx
+++ webapp/frontend/src/pages/index.tsx
@@ -13,7 +13,7 @@ export default function Home() {
               "Can an AI win Ghana's National Science and Maths Quiz?. This is the question posed by the NSMQ AI Grand Challenge which is an AI Grand Challenge for Education using Ghana's National Science and Maths Quiz competition (NSMQ) as a case study."
             }
             {
-              "The goal of nsmqai is build an AI to compete live in the NSMQ competition and win — performing better than the best contestants in all rounds and stages of the competition."
+              "The goal of nsmqai is to build an AI to compete live in the NSMQ competition and win — performing better than the best contestants in all rounds and stages of the competition."
             }
           </p>
         </div>
