e1235d84d819d961722640fe6f031f870b6ca33c
Jimmy Essel <jimmy.essel@kibo.school>
2023-08-04 17:45:37 +0000

    Changed results from csv to txt

--- data-preprocessing/JSON_Section_Extractor/main.py
+++ data-preprocessing/JSON_Section_Extractor/main.py
@@ -6,7 +6,7 @@ import csv
 
 def read_PATH_json():
     with open('bookPaths.json', 'r') as file:
-        return json.load(file)['path_to_parsed_books']
+        return json.load(file)
 
 # search for json files in the directory and add their names to a list
 def put_json_filenames_in_list(parsed_books_path):
@@ -52,24 +52,29 @@ def json_to_section(json_dictionary, nested_list=False):
             except:
                 continue
 
-def write_sections_to_csv(file, pages, sections):
+
+def create_directory(directory):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+        print(f"Directory '{directory}' created successfully.")
+    else:
+        pass
+
+    # Open the CSV file in write mode
+def write_sections_to_txt(file, list1, list2, directory):
     # to obtain the filename without the end extension '.json', the last five(5) characters from the filename must be stripped off...
     # for eg: Algebra.json can be changed to Algebra by using Algebra.json[:-5], with the -5 taking care of the last five characters.
-        
-    filename = f"{file[:-5]}.csv"
-        # Open the CSV file in write mode
-    with open(filename, 'w', newline='') as file:
-        writer = csv.writer(file)
-        
-        # Write the headers
-        writer.writerow(['PAGE', 'SECTION'])
-        
-        # Write the data row by row
-        for i in range(len(pages)):
-            writer.writerow([pages[i], sections[i]])
+    filename = f"{file[:-5]}.txt"
+    file_path = os.path.join(directory, filename)
+    with open(file_path, 'w') as file:
+        for item1, item2 in zip(list1, list2):
+            file.write(f"{item1}\t{item2}\n")
 
 
-parsed_books_path = read_PATH_json()
+
+
+parsed_books_path = read_PATH_json()['path_to_parsed_books']
+results_path = read_PATH_json()['directory']
 all_json_files = put_json_filenames_in_list(parsed_books_path)
 
 
@@ -88,5 +93,6 @@ if __name__ == '__main__':
             json_to_section(json_dict) # return all sections using the second json structure as the standard.
 
         # write results to csv file
-        write_sections_to_csv(file, pages, sections)
+        create_directory(results_path)
+        write_sections_to_txt(file, pages, sections, results_path)
 
