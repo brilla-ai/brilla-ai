c9bc2ea37974ce80e0429d7bd4e8436e9282faa2
William Edor <wedor3@gatech.edu>
2023-08-21 07:56:59 -0400

    - added ability to accept a live video link and display the video - added ability to extract audio from live video stream in 5s chunks

--- streamlitDemo/pages/6_✨_NSMQ_AI_Live_Quiz.py
+++ streamlitDemo/pages/6_✨_NSMQ_AI_Live_Quiz.py
@@ -1,28 +1,26 @@
 import streamlit as st
-from utility import ai_operation, DEMO_AUDIO_5_PATH, DEMO_VIDEO_5_PATH
+from utility import get_url_manifest, extract_audio_from_live_stream
 
 st.set_page_config(page_title="NSMQ AI Live Quiz", page_icon="✨", layout="wide")
 
 st.markdown("# NSMQ AI Live Quiz ✨")
 st.write(
     """
-    Select a live video option to see the NSMQ AI in action against a live quiz!
+    See the NSMQ AI in action against a live quiz!
     """
 )
 
 st.divider()
 
-# utilizing 5 in order to align the 2nd column closer to the 1st
-nsmqGhanaCol, joyPrimeCol = st.columns([1,5])
+liveVideoURL = st.text_input('Live Video Link')
 
-liveVideoVal = ''
+if st.button('Submit'):
+    vidCol, aiOpsCol = st.columns([2,2])
 
-with nsmqGhanaCol:
-    if st.button('NSMQ GHANA'):
-        liveVideoVal = 'https://www.youtube.com/embed/mNR8aHWE3Mc'
+    with vidCol:
+        st.video(liveVideoURL, start_time=0)
 
-with joyPrimeCol:
-    if st.button('JOY PRIME'):
-        liveVideoVal = 'https://www.youtube.com/embed/PG2HPAPkaaw'
-
-st.components.v1.iframe(liveVideoVal, width=550, height=400)
+    with aiOpsCol:
+        urlManifest = get_url_manifest(liveVideoURL)
+        with st.spinner('Transcribing'):
+            extract_audio_from_live_stream(urlManifest)
