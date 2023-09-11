f943c7bd8060801ebc5948944a3f44e303e70a71
Mark Owusu Ayim <52895824+markowusu@users.noreply.github.com>
2023-09-11 02:31:21 +0100

    Update README.md (#75)
    * Update README.md
    
    Streamlined project configuration for the web application by offering precise instructions for establishing an environment.
    
    * Update README.md
    
    * Fix: file not found error 
    
    subprocess.Popen() method throws an error  message when command is executed on a macbook
    
    * provided links to the given notebooks README.md
    
    * inserted an image for the API setup page and updated the ReadMe with the uploaded image
    
    * Deleted duplicate statements
    
    * updated ReadMe
    
    * removed gitignore files from repo

--- streamlitDemo/start_nsmqai.py
+++ streamlitDemo/start_nsmqai.py
@@ -9,12 +9,12 @@ headerString = '''
 print(headerString)
 print("### Starting up NSMQ AI ###")
 print("### Step 1: Installing essential dependencies ###")
-run_dep = subprocess.Popen('pip install -q -r requirements.txt')
+run_dep = subprocess.Popen('pip install -q -r requirements.txt', shell= True)
 run_dep.wait()
 
 if run_dep.returncode == 0:
     print("### Step 2: Starting Up App ###")
-    start_app = subprocess.Popen('streamlit run NSMQ_AI.py')
+    start_app = subprocess.Popen('streamlit run NSMQ_AI.py', shell= True)
     start_app.wait()
 else:
     print("Failed to install dependencies. Please resolve and try again.")
