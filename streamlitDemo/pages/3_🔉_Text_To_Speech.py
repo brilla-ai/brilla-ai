25ef1ec9d8fd720287ca3a8655efbff45fc5cc14
KristenAP <kristenfaprempeh@gmail.com>
2023-10-17 19:04:29 +0000

    Modified pages/Text_To_Speech:dropdown text option

--- streamlitDemo/pages/3_🔉_Text_To_Speech.py
+++ streamlitDemo/pages/3_🔉_Text_To_Speech.py
@@ -22,7 +22,25 @@ errorMsg = st.empty()
 voice = st.radio('Select A Voice', (TTS_VOICE_BANK['voice1'], TTS_VOICE_BANK['voice2']), horizontal=True)
 
 # text input
-text = st.text_area("Enter Text", '', height = 80)
+#text = st.text_area("Enter Text", '', height = 80)
+
+# text input now a dropdown
+
+text= st.selectbox(
+'Select Text',
+(
+"I am the set of all possible positions a point can take. I may be a region in a plane or in space, but more often I am a continuous curve.Who am I?",
+"One particular element out of about the hundred or so elements in the Periodic Table is always present during my operations. I am the secret behind the relatively high boiling point of water. Who am i?",
+"Welcome to the national science and maths quiz.", "I am not one who likes much talk. I prefer action, so I'm on the side of forces. I am available when you need to turn things around and rotate objects about a point. Who am I?"
+    
+)
+)
+
+#this fxn inherently does not allow you to leave box empty
+#apostrophe issues identified-how to make it read contracted words
+
+st.write('You selected:', text )
+
 
 # generated audio file
 if st.button('Get Generated Speech!'):
