399247f66d414a8b8e2eaaf1f0f3a2edf1b8b3d7
Kristen Agyeman-Prempeh <48865208+KristenAP@users.noreply.github.com>
2023-10-18 10:09:14 +0000

    Modified pages/Text_To_Speech:dropdown text option (#85)
    * Modified pages/Text_To_Speech:dropdown text option
    
    * Update 3_🔉_Text_To_Speech.py

--- streamlitDemo/pages/3_🔉_Text_To_Speech.py
+++ streamlitDemo/pages/3_🔉_Text_To_Speech.py
@@ -21,8 +21,20 @@ errorMsg = st.empty()
 # choose voice options
 voice = st.radio('Select A Voice', (TTS_VOICE_BANK['voice1'], TTS_VOICE_BANK['voice2']), horizontal=True)
 
-# text input
-text = st.text_area("Enter Text", '', height = 80)
+# text input-dropdown
+
+text= st.selectbox(
+'Select Text',
+(
+"I am the set of all possible positions a point can take. I may be a region in a plane or in space, but more often I am a continuous curve.Who am I?",
+"One particular element out of about the hundred or so elements in the Periodic Table is always present during my operations. I am the secret behind the relatively high boiling point of water. Who am i?",
+"Welcome to the national science and maths quiz.", "I am not one who likes much talk. I prefer action, so I'm on the side of forces. I am available when you need to turn things around and rotate objects about a point. Who am I?"
+    
+)
+)
+
+st.write('You selected:', text )
+
 
 # generated audio file
 if st.button('Get Generated Speech!'):
