cdf1a31f67e56d197f766f9332834915943c6dfd
George Boateng <georgeboateng93@gmail.com>
2023-10-26 23:39:22 +0200

    added new video and audio (#89)
    Co-authored-by: Boateng  George <boatengg@mtec-als-dock-1-126.ethz.ch>

--- streamlitDemo/utility.py
+++ streamlitDemo/utility.py
@@ -28,6 +28,7 @@ DEMO_VIDEO_3_PATH  = VIDEO_DIR  + 'video3.mp4'
 DEMO_VIDEO_4_PATH  = VIDEO_DIR  + 'video4.mp4'
 DEMO_VIDEO_5_PATH  = VIDEO_DIR  + 'video5.mp4'
 DEMO_VIDEO_6_PATH  = VIDEO_DIR  + 'video6.mp4'
+DEMO_VIDEO_7_PATH  = VIDEO_DIR  + 'video7.mp4'
 
 # AUDIO PATHS
 DEMO_AUDIO_1_PATH  = AUDIO_DIR + 'audio_video1.mp3'
@@ -36,6 +37,7 @@ DEMO_AUDIO_3_PATH  = AUDIO_DIR + 'audio_video3.mp3'
 DEMO_AUDIO_4_PATH  = AUDIO_DIR + 'audio_video4.mp3'
 DEMO_AUDIO_5_PATH  = AUDIO_DIR + 'audio_video5.mp3'
 DEMO_AUDIO_6_PATH  = AUDIO_DIR + 'audio_video6.mp3'
+DEMO_AUDIO_7_PATH  = AUDIO_DIR + 'audio_video7.mp3'
 
 # API ENDPOINTS
 STT_API_KEY = 'STT_API'
