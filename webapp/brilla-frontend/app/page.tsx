eb117e44160662a9c6f50d041f2aca3a37e1875a
Mark Owusu <markowusuayim@gmail.com>
2024-05-26 14:25:27 +0100

    merge changes from the remote repo

--- webapp/brilla-frontend/app/page.tsx
+++ webapp/brilla-frontend/app/page.tsx
@@ -5,10 +5,6 @@ import Navbar from "@/components/navbar";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col">
-      <Navbar gradientBg={false} />
-      <div>Live is meant to be easy, bruh!</div>
-
-      <Footer />
      <Navbar gradientBg = {false} />
       <div>
       <AnswerBox chatHistory={["Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae aliquam quam. Nulla facilisi. Integer ac dapibus libero, eu efficitur purus. Nam consectetur venenatis libero, in rutrum ex. Vestibulum nec est tortor"
