3ab53fae98a23b65546428a8196121d4911381ef
devfrankduah <frankduah0@gmail.com>
2024-05-30 05:25:54 +0000

    --use next js' dynamic import with { ssr: false } to ensure ReactPlayer is only loaded on the client side, which can help with server-side rendering performance. -- added shimmer to component before mounted/rendered -- responsiveness fixes and minor refactor

--- webapp/brilla-frontend/components/videoplayer.tsx
+++ webapp/brilla-frontend/components/videoplayer.tsx
@@ -1,13 +1,28 @@
 "use client";
-import React, { FC } from "react";
-import ReactPlayer from "react-player/youtube";
+import React, { FC, useState, useEffect } from "react";
+import dynamic from "next/dynamic";
+// import Spinner from "./ui/spinner";
+import Shimmer from "./ui/shimmer";
 
 interface VideoPlayerProps {
   url: string;
 }
+
+const ReactPlayer = dynamic(() => import("react-player/youtube"), {
+  ssr: false,
+});
+
 const VideoPlayer: FC<VideoPlayerProps> = ({ url }) => {
+  const [loading, setLoading] = useState(true);
+
+  const handleReady = () => {
+    setLoading(false);
+  };
+
   return (
-    <div className="flex flex-col md:flex-row md:max-w-[45vw] h-[28vh]">
+    <div className="flex flex-col md:flex-row md:max-w-[45vw] h-[28vh] relative">
+      {/* {loading && <Spinner />} */}
+      {loading && <Shimmer />}
       <ReactPlayer
         url={url}
         width={"100%"}
@@ -19,8 +34,8 @@ const VideoPlayer: FC<VideoPlayerProps> = ({ url }) => {
         muted={false}
         volume={0.5}
         playing={true}
+        onReady={handleReady}
       />
-      <source src={url} type="video/mp4" />
     </div>
   );
 };
