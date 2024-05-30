3fbce781056dcdf40dab93cb5d6dab0414ff1830
devfrankduah <frankduah0@gmail.com>
2024-05-30 05:21:44 +0000

    ----added shimmer component

---
+++ webapp/brilla-frontend/components/ui/shimmer.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Shimmer: React.FC = () => {
+  return (
+    <div className="absolute top-0 left-0 w-full h-[28vh] md:h-[65vh] bg-gray-300 animate-pulse"></div>
+  );
+};
+
+export default Shimmer;
