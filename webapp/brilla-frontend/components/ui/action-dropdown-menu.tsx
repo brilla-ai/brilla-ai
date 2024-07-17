a8ad0abbfc3f6ab1d37f88fec3a920390271ff10
Kwabena Agyapong <antwiagyapong12@gmail.com>
2024-07-17 15:50:51 +0000

    test: stop and delete live video

--- webapp/brilla-frontend/components/ui/action-dropdown-menu.tsx
+++ webapp/brilla-frontend/components/ui/action-dropdown-menu.tsx
@@ -17,14 +17,16 @@ export function ActionDropdown({ children, actions }: DropdownProps) {
   return (
     <>
       <DropdownMenu>
-        <DropdownMenuTrigger>{children}</DropdownMenuTrigger>
+        <DropdownMenuTrigger data-testid="action-trigger">
+          {children}
+        </DropdownMenuTrigger>
         <DropdownMenuContent>
-          {actions.map(({ action, onClickFn }) => (
+          {actions.map(({ action, onClickFn }, index) => (
             <DropdownMenuItem
               key={action}
               className="py-2 px-3 text-xs rounded-[4px] min-w-[116px] hover:outline-none hover:cursor-pointer hover:bg-blue-50 outline-none"
               onSelect={onClickFn}
-              data-testid="dropdown-menu-item"
+              data-testid={`dropdown-menu-item-${index}`}
             >
               {action}
             </DropdownMenuItem>
