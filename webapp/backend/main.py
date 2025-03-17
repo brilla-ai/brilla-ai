3b409ffd7e00434442d304aea93d76d3da228bf5
markowusu <markowusuayim@gmail.com>
2025-03-17 17:46:27 +0100

    add: added  docker implementation for both the  backend and the frontend app

--- webapp/backend/main.py
+++ webapp/backend/main.py
@@ -1,3 +1,6 @@
+import asyncio
+from contextlib import asynccontextmanager
+import threading
 from typing import Annotated
 from fastapi import Depends, FastAPI
 from uvicorn import run
@@ -12,15 +15,42 @@ import os
 from database import Base, engine, get_db
 from dotenv import load_dotenv
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.interval import IntervalTrigger
 from helper.seed_data import Seed
 from job.video_job import check_and_update_live_video_status
 
-app = FastAPI()
 
-scheduler = AsyncIOScheduler()
-scheduler.add_job( check_and_update_live_video_status, IntervalTrigger(seconds=60))
-scheduler.start()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    global loop
+    loop = asyncio.get_running_loop()  # ✅ Store FastAPI's event loop at startup
+    scheduler.add_job(job_wrapper, trigger=IntervalTrigger(seconds=10))
+    scheduler.start()
+    print("Scheduler started.")
+    yield
+    scheduler.shutdown()
+    print("Scheduler stopped.")
+    
+app = FastAPI(lifespan=lifespan)
+
+scheduler = BackgroundScheduler()
+loop = None 
+
+
+def job_wrapper():
+    global loop
+    if loop:
+        future = asyncio.run_coroutine_threadsafe(check_and_update_live_video_status(), loop)
+        future.result() 
+
+# def start_scheduler():
+#     scheduler.add_job(
+#         lambda: asyncio.get_event_loop(check_and_update_live_video_status()), 
+#         trigger=IntervalTrigger(seconds=60)
+#     )
+#     scheduler.start()
 
 app.add_middleware(
     CORSMiddleware,
@@ -58,6 +88,7 @@ def seed_data():
     db = next(get_db())
     seed = Seed(db)
     seed.seed_ai_operations()
+
 
 
 @app.on_event("shutdown")
